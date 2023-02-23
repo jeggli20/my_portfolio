2e79064172502b9687a61fbd66b8049dac71cae8
jeggli20 <76450500+jeggli20@users.noreply.github.com>
2023-02-23 12:55:16 -0700

    Initial Commit and Layout Structure
    Created my React app and started working on the layout component with its structure.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Layout from "./components/layout/Layout";
+
+const App = () => {
+  return (
+    <Layout>
+      <main>
+        <h1>Jacob Eggli</h1>
+      </main>
+    </Layout>
+  );
+};
+
+export default App;
