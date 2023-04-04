363a77a2f2f38586a300a90de0c8e786a95c1016
jeggli20 <76450500+jeggli20@users.noreply.github.com>
2023-04-04 13:52:21 -0600

    Email Restructuring
    Restructured my backend logic to send an email out.

--- pages/api/email.js
+++ pages/api/email.js
@@ -4,44 +4,35 @@ dotenv.config();
 
 const user = process.env.EMAIL_USER;
 const pass = process.env.EMAIL_PASSWORD;
-const clientId = process.env.OAUTH_CLIENT_ID;
-const clientSecret = process.env.OAUTH_CLIENT_SECRET;
-const refreshToken = process.env.OAUTH_REFRESH_TOKEN;
 
 let transporter = createTransport({
   service: "Gmail",
   auth: {
-    type: "OAuth2",
     user: user,
     pass: pass,
-    clientId: clientId,
-    clientSecret: clientSecret,
-    refreshToken: refreshToken,
   },
 });
 
 const handler = async (req, res) => {
-  if (req.method === "POST") {
-    let mailOptions = {
-      from: process.env.EMAIL_USER,
-      to: process.env.EMAIL_USER,
-      subject: `Jacob Eggli - Web Development: ${req.body.name}`,
-      text: req.body.message,
-    };
+  try {
+    if (req.method === "POST") {
+      let mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: process.env.EMAIL_USER,
+        subject: `Jacob Eggli - Web Development: ${req.body.name}`,
+        text: req.body.message,
+      };
 
-    transporter.sendMail(mailOptions, function (err, data) {
-      if (err) {
-        res.status(500).json({
-          status: "fail",
-        });
-      } else {
-        console.log("== Message sent ==");
-        res.status(200).json({ status: "success" });
-      }
-    });
+      console.log("Mail Options Set");
 
-    res.status(200).json({});
+      transporter.sendMail(mailOptions);
+    }
+  } catch (error) {
+    res.status(500).send({ status: "fail" });
+    return;
   }
+
+  res.status(200).json({ status: "success" });
 };
 
 export default handler;
